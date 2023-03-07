0b2362b2c5504926b16c99419810bc7738aba29a
NeemaJanet <123103378+NeemaJanet@users.noreply.github.com>
2023-03-07 15:53:35 +0300

    Update mygrade.js

--- mygrade.js
+++ mygrade.js
@@ -3,24 +3,29 @@ let marks = prompt("Enter Marks");
 alert(marks);
 if (x > 0 && x < 101)
 {
-    if(x > 80)
+    if(x > 70)
     {
         alert("A");
         console.log("Value when A");
     }
-    else if(x < 70 )
+    else if(x > 70 && x < 60 )
     {
         alert("B"); 
 
     }
-    else if(x < 60)
+    else if(x > 60 && x < 50)
     {
         alert("C"); 
 
     }
-    else if(x < 50)
+    else if(x > 50 && x < 40)
     {
         alert("D"); 
+
+    }
+    else if(x > 30)
+    {
+        alert("E"); 
 
     }
 }
